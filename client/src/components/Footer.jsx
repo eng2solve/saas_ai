b66ae3f05a6a282bdb03d6a35f1ca3a29c5f30aa
eng2solve <durgesh71199@gmail.com>
2025-08-05 15:58:21 +0530

    push check

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer =()=> {
                 </div>
             </div>
             <p className="pt-4 text-center text-xs md:text-sm pb-5">
-                Copyright 2025 © Durgesh Private Limited All Right Reserved.
+                Copyright 2025 ©Durgesh Private Limited All Right Reserved.
             </p>
         </footer>
     );
